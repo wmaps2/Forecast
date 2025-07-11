b2c7fcf13f8c6d4ed0b40905374e2f6904f4b45c
William Phillips <wmaps2@gmail.com>
2025-07-11 12:25:52 -0400

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from prophet import Prophet
 import plotly.graph_objects as go
 from pandas.tseries.frequencies import to_offset
 
-st.title("📈 Sales Forecast with P50 / P90")
+st.title("📈 Sales Forecast con P50 / P90")
 
 # Upload CSV file
 file = st.file_uploader("Upload a CSV file with 'ds' (date) and 'y' (sales)", type="csv")
